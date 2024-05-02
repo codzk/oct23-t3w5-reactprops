37a02c3d2e85d2069f9c6ba899e0733e4b4176d3
codzk <zekemilne@gmail.com>
2024-05-03 09:25:07 +1000

    feat: movielist components in place

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,15 @@
-import './stylesheets/App.css';
+import '../stylesheets/App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <h1>Favorite Movies</h1>
+
+      {/* <MovieList /> */}
+
+
+
       
     </div>
   );
