bcc4204d16c7b6becb82c51e64c7ec0be767d773
codzk <zekemilne@gmail.com>
2024-05-03 16:21:43 +1000

    feat: implemented react-bootstrap

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 import MovieList from './components/MovieList';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
 
-    <app>
-      
+    <App>
+
       <MovieList />
 
-    </app>
+    </App>
   </React.StrictMode>
 );
 
